1ec401e3bb503f13f7b16d587c650bb652c5be22
sabrina <sabrinaahmednitu@gmail.com>
2024-05-22 08:53:26 +0600

    some change

--- src/app/product/product.route.ts
+++ src/app/product/product.route.ts
@@ -8,10 +8,10 @@ router.post('/', productControllers.createProduct)
 //2. Retrieve a List of All Products (Method: GET) route
 router.get('/', productControllers.getAllOrSearchProducts)
 
-//3. Retrieve a Specific Product by ID (Method: GET) route
+//3. Retrieve a Specific Product by ID (Method: GET) router
 router.get('/:productId', productControllers.getProductById)
 
- //4. Update Product Information (Method: PUT) route
+ //4. Update Product Information (Method: PUT) router
 router.delete('/:productId', productControllers.deleteProduct)
 
 //5. delete a product by its ID (Method: DELETE) route
